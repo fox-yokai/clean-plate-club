eb423c0f97ea64898567242e9b2d10fb7310e577
fox-yokai <pat.w.cain@gmail.com>
2020-07-11 14:04:06 -0500

    mongoose connection to db added to server.js

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 
 const PORT = 5000;
 
+// Connect to the Mongo DB
+await mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/babish",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+
 // Start the Express server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
